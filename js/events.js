dabc1a14d96ec73ed9c7483b1ebeab813d6b3934
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-21 22:37:08 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt(){
+  $('p').on('click', ()=>{
+    alert("Hey!");
+  })
+}
+function frameIt(){
+  $('img').on('load', ()=>{
+    $('img').addClass('tasty');
+  })
+}
+function pressIt(){
+  $('#typing').on('keydown', (e)=>{
+    if (e.which === 71){
+      alert("the g key was pressed");
+    }
+  })
+}
+function submitIt(){
+  $(document).on('submit', ()=>{
+    alert("Your form is going to be submitted now.");
+  })
+}
 
+$(document).ready(function(){
+  getIt(); frameIt(); pressIt(); SubmitIt();
 // call functions here
 
 });
